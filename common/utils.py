26a3c99f7dec460eb0ce09a5d8c6fde0157884c4
Phan Văn Tài <taiphanvan2403@gmail.com>
2024-03-06 15:24:06 +0000

    [PBL5] Correct the keypoints extraction function

--- common/utils.py
+++ common/utils.py
@@ -4,3 +4,10 @@ def extract_parts_of_file(image_path):
     """
     file_name, file_extension = image_path.split(".")
     return file_name, file_extension
+
+
+def get_current_time_string():
+    from datetime import datetime
+
+    current_time = datetime.now()
+    return f"{current_time.year}{current_time.month}{current_time.day}{current_time.hour}{current_time.minute}{current_time.second}{current_time.microsecond}"
