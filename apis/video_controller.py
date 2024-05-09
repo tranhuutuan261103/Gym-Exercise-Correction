ad78c747065da675e31404f07fd7bdcd9daa24b4
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-05-09 08:46:32 +0700

    feat: [PBL5] Handle uploading chunks

--- apis/video_controller.py
+++ apis/video_controller.py
@@ -12,6 +12,30 @@ def allowed_file(filename) -> bool:
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@video_bp.route("upload_chunks/<exercise_type>", methods=["POST"])
+def upload_video_chunks(exercise_type):
+    try:
+        if "file" not in request.files:
+            return jsonify({"error": "No file part"}), 400
+
+        user_id = request.form.get("userId")
+        formatted_time = request.form.get("formattedTime")
+        chunk_index = request.form.get("chunkIndex")
+        total_chunks = request.form.get("totalChunks")
+        chunk = request.files["file"]
+
+        # Kiểm tra xem chunk có tồn tại không
+        if chunk is None:
+            return jsonify({"error": "No file provided"}), 400
+        chunk = chunk.read()
+        return video_model.handle_upload_chunks(
+            user_id, exercise_type, chunk, chunk_index, total_chunks, formatted_time
+        )
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 @video_bp.route("upload/<exercise_type>", methods=["POST"])
 def handle_uploaded_video(exercise_type):
     if "file" not in request.files:
@@ -26,5 +50,5 @@ def handle_uploaded_video(exercise_type):
     if not (file and allowed_file(file.filename)):
         return jsonify({"error": "File type not allowed"}), 400
 
-    response_info = video_model.handle_uploaded_video(exercise_type, file, user_id)
+    response_info = video_model.handle_uploaded_video(user_id, exercise_type, file)
     return response_info
--- apis/video_model.py
+++ apis/video_model.py
@@ -2,12 +2,12 @@ import os, cv2, time, pytz
 from flask import jsonify
 from werkzeug.utils import secure_filename
 from ML_models.plank.PlankModel import PlankModel
+from datetime import datetime
 from utils import (
     combine_frames_to_video,
     create_folder_if_not_exists,
     create_thread_and_start,
 )
-from datetime import datetime
 from firebase_utils import (
     initialize_firestore,
     upload_file_to_fire_storage,
@@ -23,22 +23,43 @@ available_models = {
 
 
 class VideoModel:
-    def handle_uploaded_video(self, exercise_type, file, user_id):
-        filename = secure_filename(file.filename)
-        current_dir = os.path.dirname(__file__)
-        current_time = datetime.now()
-        formatted_time = current_time.strftime("%Y%m%d%H%M%S")
-
-        # Tạo task để thêm video vào Firestore
-        record_id = self.init_empty_handle_video_record(user_id)
-
+    def handle_upload_chunks(
+        self, user_id, exercise_type, chunk, chunk_index, total_chunks, formatted_time
+    ):
         create_folder_if_not_exists(f"{current_dir}/{UPLOADED_VIDEOS}")
         uploaded_video_url = (
-            f"{current_dir}/{UPLOADED_VIDEOS}/{user_id}_{formatted_time}_{filename}.mp4"
+            f"{current_dir}/{UPLOADED_VIDEOS}/{user_id}_{formatted_time}.mp4"
         )
+        with open(f"{uploaded_video_url}", "wb") as f:
+            f.write(chunk)
 
-        # Lưu video xuống thư mục uploaded_videos
-        file.save(uploaded_video_url)
+        if int(chunk_index) == int(total_chunks) - 1:
+            response_info = self.handle_uploaded_video(
+                user_id, exercise_type, uploaded_video_url=uploaded_video_url
+            )
+            return response_info
+        return jsonify({"message": f"Chunk {chunk_index} uploaded successfully"}), 200
+
+    def handle_uploaded_video(
+        self, user_id, exercise_type, file=None, uploaded_video_url=None
+    ):
+        """
+        Xử lý video đã được upload lên server khi đã thu thập đủ các chunks
+        """
+        # Tạo task để thêm video vào Firestore
+        record_id = self.init_empty_handle_video_record(user_id, exercise_type)
+
+        if uploaded_video_url is None:
+            filename = secure_filename(file.filename)
+            current_dir = os.path.dirname(__file__)
+            current_time = datetime.now()
+            formatted_time = current_time.strftime("%Y%m%d%H%M%S")
+
+            create_folder_if_not_exists(f"{current_dir}/{UPLOADED_VIDEOS}")
+            uploaded_video_url = f"{current_dir}/{UPLOADED_VIDEOS}/{user_id}_{formatted_time}_{filename}.mp4"
+
+            # Lưu video xuống thư mục uploaded_videos
+            file.save(uploaded_video_url)
 
         if exercise_type in available_models:
             response_info = available_models.get(exercise_type).plank_detection_offline(
@@ -70,7 +91,7 @@ class VideoModel:
         else:
             return jsonify({"error": "Exercise type not found"}), 400
 
-    def init_empty_handle_video_record(self, user_id):
+    def init_empty_handle_video_record(self, user_id, exercise_type):
         handled_offline_videos_ref = db.collection("HandledOfflineVideos").document()
         timezone = pytz.timezone("Asia/Ho_Chi_Minh")
         handled_offline_videos_ref.set(
@@ -78,6 +99,7 @@ class VideoModel:
                 "UserId": user_id,
                 "HandledVideoUrl": "",
                 "UploadedAt": datetime.now(timezone),
+                "ExerciseType": exercise_type,
             }
         )
         return handled_offline_videos_ref.id
