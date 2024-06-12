14966d40e9e3011044510cc5c9728a6a761ec0db
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-06-13 02:04:14 +0700

    feat: [PBL5] Send notification to mobile app

--- desktop_app/components/Home.py
+++ desktop_app/components/Home.py
@@ -190,7 +190,8 @@ class Home(tk.Frame):
     def stop_canvas(self):
         self.is_running = False  # Pause updating the webcam view
         # Sử dụng hàm send_push_notification để gửi thông điệp đến thiết bị có token tương ứng
-        # send_push_notification("Finish the exercise", "You have finished the exercise, please check the result")
+        send_push_notification('Finish the exercise', 
+                               f'You have finished the exercise {self.activitie_selected}, please check the result')
 
     def start_canvas(self):
         if self.video_capture is None or not self.video_capture.isOpened():
